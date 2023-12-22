d6a62e4fcfcba38b23658efcef14c8946d829e91
TheW3bDevs <154611077+TheW3bDevs@users.noreply.github.com>
2023-12-22 21:18:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post("/postMail", async (req, res) => {
 
   const mailOptions = {
     from: "Sai@1234567",
-    to: "prasaddurga2031@gmail.com",
+    to: "staarfoods.com@gmail.com",
     subject: "Order Details",
     html: `
     <div
@@ -88,6 +88,74 @@ app.post("/postMail", async (req, res) => {
   </div>
     `,
   };
+
+  const mailOptions2 = {
+    from: "Sai@1234567",
+    to: user.email,
+    subject: "Order Details",
+    html: `
+    <div
+    style="
+      font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS',
+        sans-serif;
+    
+  >
+    <h2 style="text-decoration: underline 2px orange;">Your Order Items</h2>
+    <div>
+
+      <table
+        style="
+          border-collapse: collapse;
+          width: 100%;
+          background: rgb(235, 234, 234);
+        "
+      >
+        <thead style="background: black; color: white; height: 50px">
+          <tr style="border-bottom: 1px solid #ddd">
+            <td style="padding: 10px"><b>Image</b></td>
+            <td style="padding: 10px"><b>Product_Name</b></td>
+            <td style="padding: 10px"><b>Quantity</b></td>
+          </tr>
+        </thead>
+        <tbody>
+        ${user.Items.map((data) => {
+          return ` <tr style="border-bottom: 1px solid #bdbcbc">
+            <td style="padding: 10px">
+              <img
+                src="${data.productImg}"
+                style="
+                  width: 100px;
+                  height: 100px;
+                  object-fit: cover;
+                  border-radius: 5px;
+                "
+                alt="Image"
+              />
+            </td>
+            <td style="padding: 10px">${data.productName}</td>
+            <td style="padding: 10px">${data.quantity}kgs</td>
+          </tr>`;
+        }).join("")}
+        </tbody>
+      </table>
+        <br />
+         <h4>Thank you, for your order soon our staff will contact you with your order.</h4>
+    
+    
+    </div>
+  </div>
+    `,
+  };
+
+  transporter.sendMail(mailOption2, (error, info) => {
+    if (error) {
+      res.json({ status: "Failed to send" });
+      console.log("Error sending email:", error);
+    } else {
+      res.json({ status: "Mail sent Successful" });
+      console.log("Email sent:", info.response);
+    }
+  });
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
